63469aff1952efc7d88e3770be98d8bd22c84a7b
Tomahawk <chris506@hotmail.co.uk>
2025-12-01 13:06:29 +0000

    Edit comments

--- chris/day01/part1.py
+++ chris/day01/part1.py
@@ -3,7 +3,7 @@
 #Read input into list of str.
 input = open("input.txt").readlines()
 
-#Convert to positive/negative int: L50 to -50, R20 to 20, etc.
+#Convert to positive/negative ints: L50 to -50, R20 to 20, etc.
 input = [-int(i[1:]) if i[0] == 'L' else int(i[1:]) for i in input]
 
 d = 50 #Dial value.
@@ -11,7 +11,7 @@ numZeroes = 0 #Puzzle answer.
 
 for i in input:
     d += i
-    #If new value is multiple of 100.
+    #If new value is 0 or multiple of 100.
     if abs(d) % 100 == 0:
         numZeroes += 1
     
