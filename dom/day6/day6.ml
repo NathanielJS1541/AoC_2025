70d20dee06967522472ecf4e2a40dbbd12636879
Dominic Mason <dominicmason555@gmail.com>
2025-12-08 12:36:35 +0000

    Day 6 part 1

--- dom/day6/day6.ml
+++ dom/day6/day6.ml
@@ -1,4 +1,4 @@
-let in_file = "test_input"
+let in_file = "input"
 
 type expr = Plus | Multiply
 type token = Num of char | Expr of expr | Whitespace | Newline | End
@@ -7,9 +7,11 @@ type parser_state = {
   input : string;
   col_num : int;
   pos : int;
-  cols : int list array;
+  cols : int list iarray;
   operators : expr list;
   number : string;
+  in_num : bool;
+  answers : int list;
 }
 
 let parse_next_char st =
@@ -24,67 +26,83 @@ let parse_next_char st =
     | _ -> raise (Failure ("Bad input:" ^ Char.escaped c))
   else End
 
-let debug_col st number =
-  let col = Array.get st.cols st.col_num in
-    let len = List.length col in
-    Printf.printf "%d + %d\n" (List.nth col (len - 1)) number
+let debug_col st number col_num =
+  Printf.printf "Col 0: [";
+  List.iter (Printf.printf "%d, ") (Iarray.get st.cols 0);
+  Printf.printf "]\nCol 1: [";
+  List.iter (Printf.printf "%d, ") (Iarray.get st.cols 1);
+  Printf.printf "]\nCol 2: [";
+  List.iter (Printf.printf "%d, ") (Iarray.get st.cols 2);
+  Printf.printf "]\nCol 3: [";
+  List.iter (Printf.printf "%d, ") (Iarray.get st.cols 3);
+  Printf.printf "]\n"
 
 let finish_number st col_num =
   let number = int_of_string st.number in
-  if st.col_num >= Array.length st.cols then (
-    let cols = Array.append st.cols [| [ number ] |] in
-    { st with number = ""; pos = st.pos + 1; col_num; cols })
+  if st.col_num >= Iarray.length st.cols then
+    let cols = Iarray.append st.cols [| [ number ] |] in
+    { st with number = ""; pos = st.pos + 1; in_num = false; col_num; cols }
   else
-    let head = Array.sub st.cols 0 st.col_num
-    and tail = Array.sub st.cols st.col_num (Array.length st.cols - st.col_num)
-    and col = List.append st.cols.(st.col_num) [ number ] in
-    debug_col st number;
-    let cols = Array.concat [ head; [| col |]; tail ] in
-    { st with number = ""; pos = st.pos + 1; col_num; cols }
+    let add_num = fun i li -> if i = st.col_num then li @ [ number ] else li in
+    let cols = Iarray.mapi add_num st.cols in
+    (*debug_col st number col_num; *)
+    { st with number = ""; pos = st.pos + 1; in_num = false; col_num; cols }
 
 let rec parse_line st =
   let token = parse_next_char st in
   match token with
   | Num c ->
-      parse_number
-        { st with pos = st.pos + 1; number = st.number ^ Char.escaped c }
+      parse_line
+        {
+          st with
+          pos = st.pos + 1;
+          number = st.number ^ Char.escaped c;
+          in_num = true;
+        }
   | Expr e ->
+      let answer =
+        match e with
+        | Plus -> List.fold_left ( + ) 0 (Iarray.get st.cols st.col_num)
+        | Multiply -> List.fold_left ( * ) 1 (Iarray.get st.cols st.col_num)
+      in
       parse_line
         {
           st with
           pos = st.pos + 1;
           operators = st.operators @ [ e ];
           col_num = st.col_num + 1;
+          answers = st.answers @ [ answer ];
         }
-  | Whitespace -> parse_line { st with pos = st.pos + 1 }
-  | Newline -> parse_line { st with pos = st.pos + 1; col_num = 0 }
-  | End -> st
-
-and parse_number st =
-  let token = parse_next_char st in
-  match token with
-  | Num c ->
-      parse_number
-        { st with pos = st.pos + 1; number = st.number ^ Char.escaped c }
-  | Expr e ->
-      parse_line
-        {
-          st with
-          pos = st.pos + 1;
-          operators = st.operators @ [ e ];
-          col_num = st.col_num + 1;
-        }
-  | Whitespace -> parse_line (finish_number st (st.col_num + 1))
-  | Newline -> parse_line (finish_number st 0)
+  | Whitespace ->
+      let new_st =
+        if st.in_num then finish_number st (st.col_num + 1)
+        else { st with pos = st.pos + 1 }
+      in
+      parse_line new_st
+  | Newline ->
+      let new_st =
+        if st.in_num then finish_number st 0
+        else { st with pos = st.pos + 1; col_num = 0 }
+      in
+      parse_line new_st
   | End -> st
 
 let day6_part1 input =
   let state =
     parse_line
-      { input; col_num = 0; pos = 0; cols = [||]; operators = []; number = "" }
+      {
+        input;
+        col_num = 0;
+        pos = 0;
+        cols = [||];
+        operators = [];
+        number = "";
+        in_num = false;
+        answers = [];
+      }
   in
-  Printf.printf "pos: %d\n" (List.fold_left ( + ) 0 state.cols.(1));
-  0
+  (* List.iter (Printf.printf "%d, ") state.answers; *)
+  List.fold_left ( + ) 0 state.answers
 
 let day6_part2 input = 0
 
